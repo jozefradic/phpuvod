b81b29a53f65902a32ca4637d024b492f9b19c49
Jojo <32668482+jozefradic@users.noreply.github.com>
2018-06-11 10:03:45 +0200

    added new funct.

--- calc.php
+++ calc.php
@@ -4,14 +4,28 @@ $num2 = $_POST['num2'];
 $func = $_POST['function'];
 $check = $_POST['checkbox'];
 $radio = $_POST['radio'];
-if (!$num1="" || !$num2="" )
+if (!isset($num1) || trim($num1) == '' || !isset($num2) || trim($num2) == '')
 {
-    die("Input is empty");
+
+    die("Input is empty");  
+    echo "test echo";  
+    exit();
+
 }
-else if (!$num1!=0 || !$num2!=0 || !$check=='false')
+else if (!$num1!=0 || !$num2!=0)
 {
 die("Number is null");
 exit();
+}
+else if (!$check=='false' || !$radio=='false'){
+    die("Please select options");
+    exit();
+}
+else if (!$radio==young || !$func=='+'){
+    $result = $_POST['num1'] + $_POST['num2'];
+}
+else if (!$radio==young || !$func=='-'){
+    $result = $_POST['num1'] - $_POST['num2'];
 }
 else
 {
@@ -29,5 +43,18 @@ switch ($func) {
         $result = $_POST['num1'] / $_POST['num2'];
 }
 echo $result;
+
+$dotaz="INSERT INTO history (valA,valB) VALUES(".$a.",".$b.")";
+$conn = mysqli_connect("localhost:3306","root","","phpcalc");
+if (!$conn) {
+    die("Connection failed: " .mysqli_connect_error());
+    }
+    echo "Connected successfully";
+    if ($conn->query($dotaz) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
+
 ?>
