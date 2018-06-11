be0285eedfc0270984df09eb1f076751c131ee12
Jojo <32668482+jozefradic@users.noreply.github.com>
2018-06-11 09:07:54 +0200

    added button

--- phpindex.php
+++ phpindex.php
@@ -16,7 +16,9 @@
 	<p align="center"><input type="checkbox" name="checkbox" id="check" value="false">Suhlasim s vypoctom</p>
 	<p align="center"><input type="radio" name="radio" value="old">Som dospely</p>
 	<p align="center"><input type="radio" name="radio" value="young">Niesom dospely</p>
-    <p align="center"><input type="submit" value="Calculate" name="calculate"></p>
+	<p align="center"><input type="submit" value="Calculate" name="calculate"></p>
+	<p align="center"><input type="reset" value="Reset" name="Reset"></p>
+	<p align="center"><button type="history" value="History" name="History">History</button> </p>
 </form>
 </body>
 </html>
